c10d7f66a1ccbb6da8f4805154311effbe4803ba
Aleks Haran <101347633+Aleks-corp@users.noreply.github.com>
2025-01-06 20:54:41 +0200

    Update saveFiles.ts

--- backend/src/helpers/saveFiles.ts
+++ backend/src/helpers/saveFiles.ts
@@ -11,23 +11,23 @@ const { S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } =
 //   secretAccessKey: AWS_SECRET_ACCESS_KEY,
 //   region: AWS_REGION,
 // });
-// const uploadToS3 = async (file: Express.Multer.File, folder: string) => {
-//   const fileContent = file.buffer; // Якщо ви використовуєте Multer з buffer
-//   const fileName = `${folder}/${uuidv4()}-${path.basename(file.originalname)}`;
+const uploadToS3 = async (file: Express.Multer.File, folder: string) => {
+  const fileContent = file.buffer; // Якщо ви використовуєте Multer з buffer
+  const fileName = `${folder}/${uuidv4()}-${path.basename(file.originalname)}`;
 
-//   const params = {
-//     Bucket: S3_BUCKET_NAME,
-//     Key: fileName,
-//     Body: fileContent,
-//     ContentType: file.mimetype,
-//   };
+  //   const params = {
+  //     Bucket: S3_BUCKET_NAME,
+  //     Key: fileName,
+  //     Body: fileContent,
+  //     ContentType: file.mimetype,
+  //   };
 
-//   try {
-//     const { Location } = await s3.upload(params).promise();
-//     return Location; // Повертаємо посилання на файл у S3
-//   } catch (err) {
-//     console.error("Помилка при завантаженні в S3:", err);
-//     throw new Error("Error uploading to S3");
-//   }
-// };
-// export default uploadToS3;
+  //   try {
+  //     const { Location } = await s3.upload(params).promise();
+  //     return Location; // Повертаємо посилання на файл у S3
+  //   } catch (err) {
+  //     console.error("Помилка при завантаженні в S3:", err);
+  //     throw new Error("Error uploading to S3");
+  //   }
+};
+export default uploadToS3;
