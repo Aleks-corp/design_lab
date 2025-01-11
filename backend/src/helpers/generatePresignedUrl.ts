41764f1169e7af407c6e81b481037127f58c6b43
Aleks Haran <101347633+Aleks-corp@users.noreply.github.com>
2025-01-11 21:17:25 +0200

    Update generatePresignedUrl.ts

--- backend/src/helpers/generatePresignedUrl.ts
+++ backend/src/helpers/generatePresignedUrl.ts
@@ -1,6 +1,6 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 
 const { S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } =
   process.env;
@@ -14,15 +14,15 @@ const s3 = new S3Client({
 });
 
 export const generatePresignedUrl = async (file: string) => {
-  const fileExtension = file.substring(file.lastIndexOf("."));
-  const newFileName = `${file.substring(
-    0,
-    file.lastIndexOf(".")
-  )}-${nanoid()}${fileExtension}`;
+  // const fileExtension = file.substring(file.lastIndexOf("."));
+  // const newFileName = `${file.substring(
+  //   0,
+  //   file.lastIndexOf(".")
+  // )}-${nanoid()}${fileExtension}`;
 
   const command = new PutObjectCommand({
     Bucket: S3_BUCKET_NAME,
-    Key: newFileName,
+    Key: file,
   });
   const signedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
   return signedUrl;
