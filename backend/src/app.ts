ec65aa7dcd8790cd7a0e79d20df65463d4a209f9
Aleks Haran <nporock@gmail.com>
2025-02-22 22:13:58 +0200

    add temp cors ip

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,11 @@ app.use(morgan(formatsLogger, { stream: accessLogStream }));
 
 app.use(
   cors({
-    origin: [process.env.FRONT_SERVER, "https://secure.wayforpay.com"],
+    origin: [
+      process.env.FRONT_SERVER,
+      "91.90.11.199/32",
+      "https://secure.wayforpay.com",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Authorization", "Content-Type"],
   })
